e17798a40fed7975cb0b33e123570368191a3d6e
MD: Fahad Hossain Khan <103371991+fhkhan007@users.noreply.github.com>
2023-03-23 10:55:45 +0600

    1.Sum of Array Plus One

---
+++ 1.Sum of Array Plus One/index.js
@@ -0,0 +1,31 @@
+/*Problem
+Write a function that takes an array of integers and returns the sum of the integers after adding 1 to each.
+*/ 
+
+/*
+function sumArray(array) {
+    const ourArray = [1, 4, 0, 9, -3];
+    let sum = 0;
+  
+    for (let i = 0; i < ourArray.length; i += 1) {
+      sum += ourArray[i];
+    }
+    
+    return sum;
+  }
+  
+  console.log(sumArray([1, 4, 0, 9, -3]));
+  */
+
+  function sumArray(array) {
+    let sum = 0;
+  
+    array.forEach(item => {
+      sum += item;
+    });
+  
+    console.log(sum);
+    return sum;
+  }
+  
+  sumArray([1, 4, 0, 9, -3]);
