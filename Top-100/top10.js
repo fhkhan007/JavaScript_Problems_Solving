1d77982fa81b094d0288540a4ac7442b79593cb2
MD: Fahad Hossain Khan <fahadhossainkhan@gmail.com>
2025-04-29 13:21:09 +0600

    factorial uoTodate

--- Top-100/top10.js
+++ Top-100/top10.js
@@ -2,9 +2,14 @@
 // Write a function to compute the factorial of a number.
 
 function fact(n) {
+
+    if(n<0) return "Factorisl not defined for negative Numbers";
+
+    if(n==1 || n==0) return 1;
+
     let num =1 ;
     for(let i=1; i<=n; i++){
-        num = num*i;
+        num = num * i ;
     }
     return num;
 }
