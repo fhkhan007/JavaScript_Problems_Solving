a1b02cb3207416eb0c86a19b00cbe9e13b942510
MD: Fahad Hossain Khan <fahadhossainkhan@gmail.com>
2025-08-03 11:29:20 +0600

    Sum of digits

--- Top-100/top10.js
+++ Top-100/top10.js
@@ -52,15 +52,46 @@ console.log(fact(5));
 
 // 4. Reverse a String . Write a function to reverse a given string.
 
-let name = 'hello';
-let result = name.split('');
-console.log(result);
+// let name = 'hello';
+// let result = name.split('');
+// console.log(result);
 
-function reverseString (str){
-    return str
-    .split('')
-    .reverse()
-    .join()
+// function reverseString (str){
+//     return str
+//     .split('')
+//     .reverse()
+//     .join()
+// }
+
+// console.log(reverseString("arman"));
+ 
+// 5. Fibonacci Sequence . Write a function to generate the Fibonacci sequence up to n terms.
+
+// function generateFibonacci(n) {
+//     if (n<0) return [];
+//     else if (n === 1) return [0];
+//     const fibSequence = [0 , 1];
+
+//     for (let i =2 ; i<n ; i++) {
+//         fibSequence.push(fibSequence[i-1] + fibSequence[i-2]);
+//     }
+
+//     return fibSequence;
+// }
+
+// 6. Sum of Digits . Write a function to calculate the sum of the digits of a number.
+
+function sumOfDigits (num) {
+    let sum=0;
+
+    num = Math.abs(num);
+
+    while(num > 0) {
+        let digit = num %10;
+        sum = sum + digit ;
+        num = Math.floor(num / 10)
+    }
+    return  sum;
 }
 
-console.log(reverseString("arman"));
+console.log(sumOfDigits(234));
